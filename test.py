0d369e5b16b058b185c1eeb4379def429073e66e
tseyongg <tseyong0987@gmail.com>
2025-03-04 22:04:52 +0800

    test print excel output

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 import requests
+import json
+import pandas as pd
+
+url = "https://raw.githubusercontent.com/Papagoat/brain-assessment/main/restaurant_data.json"
+response = requests.get(url)
+data = response.json()
+
+country_codes_df = pd.read_excel("Country-Code.xlsx")
+country_codes = dict(zip(country_codes_df['Country Code'], country_codes_df['Country']))
+
+print(country_codes)
